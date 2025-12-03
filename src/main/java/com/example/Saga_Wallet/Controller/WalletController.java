34f9f918a6a1cf9dc1204ed2354561eb456d794a
Arpit Chauhan <arpitchauhan@Arpits-MacBook-Air.local>
2025-12-03 20:18:18 +0530

    Added the Dtos and Controller

---
+++ src/main/java/com/example/Saga_Wallet/Controller/WalletController.java
@@ -0,0 +1,62 @@
+package com.example.Saga_Wallet.Controller;
+
+import com.example.shardedsagawallet.dtos.CreateWalletRequestDTO;
+import com.example.shardedsagawallet.dtos.CreditWalletRequestDTO;
+import com.example.shardedsagawallet.dtos.DebitWalletRequestDTO;
+import com.example.shardedsagawallet.entities.Wallet;
+import com.example.shardedsagawallet.services.WalletService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/wallets")
+@Slf4j
+public class WalletController {
+    private final WalletService walletService;
+
+    @PostMapping
+    public ResponseEntity<Wallet> createWallet(@RequestBody CreateWalletRequestDTO request) {
+        try {
+            Wallet newWallet = walletService.createWallet(request.getUserId());
+            return ResponseEntity.status(HttpStatus.CREATED).body(newWallet);
+        } catch (Exception e) {
+            log.error("Error creating wallet", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Wallet> getWalletById(@PathVariable Long id) {
+        Wallet wallet = walletService.getWalletById(id);
+        return ResponseEntity.ok(wallet);
+    }
+
+    @GetMapping("/{id}/balance")
+    public ResponseEntity<BigDecimal> getWalletBalance(@PathVariable Long id) {
+        BigDecimal balance = walletService.getWalletBalance(id);
+        return ResponseEntity.ok(balance);
+    }
+
+    @PostMapping("/{userId}/debit")
+    public ResponseEntity<Wallet> debitWallet(@PathVariable Long userId, @RequestBody DebitWalletRequestDTO request) {
+        walletService.debit(userId, request.getAmount());
+        Wallet wallet = walletService.getWalletByUserId(userId);
+        return ResponseEntity.ok(wallet);
+    }
+    
+    @PostMapping("/{userId}/credit")
+    public ResponseEntity<Wallet> creditWallet(@PathVariable Long userId, @RequestBody CreditWalletRequestDTO request) {
+        walletService.credit(userId, request.getAmount());
+        Wallet wallet = walletService.getWalletByUserId(userId);
+        return ResponseEntity.ok(wallet);
+    }
+    
+    
+}
