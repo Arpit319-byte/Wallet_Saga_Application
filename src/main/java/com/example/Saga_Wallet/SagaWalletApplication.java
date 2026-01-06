4498b4770a925c2c26b2db3110f56ca9e1cf98d3
Arpit Chauhan <arpitchauhan@Arpits-MacBook-Air.local>
2026-01-06 13:02:51 +0530

    False commit

--- src/main/java/com/example/Saga_Wallet/SagaWalletApplication.java
+++ src/main/java/com/example/Saga_Wallet/SagaWalletApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SagaWalletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SagaWalletApplication.class, args);
+
+        SpringApplication.run(SagaWalletApplication.class, args);
 	}
 
 }
